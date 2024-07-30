afa884eecdd89532c28f5435bd704aeedd382f75
lore1379 <lorenzo.g92@gmail.com>
2024-07-30 11:38:17 -0400

    implemented read_account

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,19 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+
+    def test_read_an_account(self):
+        """ It should Read a single Account """
+        test_account = self._create_accounts(1)[0]
+        response = self.client.get(
+            f"{BASE_URL}/{test_account.id}", content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], test_account.name)
+
+    def test_account_not_found(self):
+        response = self.client.get(
+            f"{BASE_URL}/0", content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
